d7632fc7aa6fd53b874bc45782296586c6bfb0ae
ElEsteeb <thestevetorres@gmail.com>
2025-03-21 16:50:57 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const navLinks = document.querySelector('.nav-links');
 if (menuToggle && navLinks) {
   menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+    menuToggle.classList.toggle('active');
   });
 }
 
