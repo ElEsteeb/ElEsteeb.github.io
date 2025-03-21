7e744d91f0c4923e63b82b613aee2d4fadaf1029
ElEsteeb <thestevetorres@gmail.com>
2025-03-21 18:25:56 -0500

    Update dark-mode.js

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,22 +1,43 @@
 try {
-  document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
     const darkModeToggle = document.querySelector('.dark-mode-toggle');
+    console.log('Dark mode toggle element:', darkModeToggle);
     if (darkModeToggle) {
       const toggleDarkMode = () => {
-        console.log('Toggling dark mode');
+        console.log('Before toggling - Body classes:', document.body.classList.toString());
         document.body.classList.toggle('dark-mode');
-        console.log('Body classes:', document.body.classList.toString());
+        document.body.classList.toggle('light-mode');
+        console.log('After toggling - Body classes:', document.body.classList.toString());
         localStorage.setItem('darkMode', document.body.classList.contains('dark-mode') ? 'enabled' : 'disabled');
       };
       darkModeToggle.addEventListener('click', toggleDarkMode);
       darkModeToggle.addEventListener('touchstart', toggleDarkMode);
+    } else {
+      console.error('Dark mode toggle button not found');
     }
 
-    // Load dark mode preference on page load
-    if (localStorage.getItem('darkMode') === 'enabled') {
-      document.body.classList.add('dark-mode');
+    const storedDarkMode = localStorage.getItem('darkMode');
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (storedDarkMode) {
+      if (storedDarkMode === 'enabled') {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+      } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+      }
+    } else {
+      if (prefersDarkMode) {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+        localStorage.setItem('darkMode', 'enabled');
+      } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+      }
     }
-  });
+  }, 100);
 } catch (e) {
   console.error('Error in dark mode toggle script:', e);
 }
